630a11c1e6d95b23dee0a5d7ad8a56707fa002bf
Prathmesh Khode <prathameshkhode@gmail.com>
2021-05-18 16:19:19 +0530

    added React Router in webapp

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import Signin from './pages/signin';
-import { getTokenFromUrl } from './services/auth';
-import SpotifyWebApi from "spotify-web-api-js";
 import Home from './pages/home';
-
-const spotify = new SpotifyWebApi();
+import Signin from './pages/signin';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
-
-  const [token,setToken] = useState(null);
-
-  //Run code based on given condition
-  useEffect(() => {
-    //Code...
-    const hash = getTokenFromUrl();
-    window.location.hash = "";
-
-    const _token = hash.access_token;
-
-    if(_token){
-      setToken(_token);
-      spotify.setAccessToken(_token);
-      spotify.getMe().then(user => {
-        console.log("The user==>>>>", user);
-      })
-    }
-  },[]);
-
   return (
-    <div className="app">
-      {
-        token ? (
-          <Home />
-        ) : (
-          <Signin />
-        )
-      }
-    </div>
+    <Router>
+      <Route path="/" exact component={Signin} />
+      <Route path="/home" exact component={Home} />
+    </Router>
   );
 }
 
